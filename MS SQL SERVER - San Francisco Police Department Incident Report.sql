f7bfc355c40b670a4407ba107b3dad68d9f69042
MihaiNetid <73075211+MihaiNetid@users.noreply.github.com>
2021-07-18 10:29:25 -0400

    Update and rename Portfolio_PD_Incident_Report.sql to MS SQL SERVER - San Francisco Police Department Incident Report.sql

--- MS SQL SERVER - San Francisco Police Department Incident Report.sql
+++ MS SQL SERVER - San Francisco Police Department Incident Report.sql
@@ -1,5 +1,6 @@
 -- analysis of real police data (Poliec_Department_Incident_reports.csv)
 -- https://data.sfgov.org/Public-Safety/Police-Department-Incident-Reports-2018-to-Present/wg3w-h783
+
 use Portfolio_Police_Department_SF;
 
 
